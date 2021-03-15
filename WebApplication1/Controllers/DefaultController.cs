042c4e562f4dfa456489fe15dd77733ec51fa89a
Mohit Pal <mohit_p_007@rediffmail.com>
2021-03-15 09:52:15 +0530

    Add project files.

---
+++ WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        productEntities db = new productEntities();
+        //public string Get()
+        //{
+        //    return "Welcome to Web API";
+        //}
+        public List<product> Get()
+        {
+            return db.products.ToList();
+        }
+    }
+}
